52388ea6636180cd4cca27cec336c5393b84afde
TharunKumarVA-WEB <vatharunkumar08@gmail.com>
2024-04-04 16:33:49 +0530

    Delete the command

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,99 +1,3 @@
-// // import React, { useEffect } from 'react';
-// // import { AiOutlineCloudDownload } from 'react-icons/ai';
-// // import { Container, Row, Col, Button } from 'react-bootstrap';
-// // import 'bootstrap/dist/css/bootstrap.min.css';
-// // import AOS from 'aos';
-// // import 'aos/dist/aos.css';
-// // import '../Style.css';
-
-// // const About = () => {
-// //     useEffect(() => {
-// //         AOS.init({ duration: 1000 });
-// //     }, []);
-
-// //     return (
-// //         <div name="about" className='w-full h-auto bg-gradient-to-b from-gray-800 to-black text-white'>
-// //             <Container fluid className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-auto'>
-// //                 <Row className='pb-8'>
-// //                     <h2 className='text-4xl font-bold inline border-b-4 border-gray-500'>About</h2>
-// //                 </Row>
-
-// //                 <Row>
-// //                     <p data-aos="slide-up" data-aos-duration="500" className='text-xl mt-5 ease-in fade-in'>
-// //                         I embarked my journey as a B.Tech Computer Science & Engineering student at Ramkrishna Mahato Government Engineering College in 2020. Along the way, I fearlessly explored various fields, including Graphic Designing, Content Creation, Freelancing, and Entrepreneurship. I delved into the world of cutting-edge technologies, such as Python, Cloud computing, React, Flutter, and Firebase. I also took my passion for technology and leadership to new heights by co-establishing Google Developer Student Clubs and the E-Cell in my college, alongside my equally enthusiastic classmates. At present, I serve GDSC RKMGEC as the Co-Lead and holds the significant responsibility of being the In-Charge of E-Cell RKMGEC.
-// //                     </p>
-// //                 </Row>
-// //                 <Row>
-// //                     <p data-aos="slide-up" data-aos-duration="500" className='text-xl mt-5 ease-in fade-in'>
-// //                         In a constant pursuit of sharing knowledge and fostering growth, I've organized numerous technical workshops, imparting valuable insights on technologies like Python, Android, and Google Cloud to aspiring learners. With a commitment to personal development and a drive for making a positive impact within the tech community.
-// //                     </p>
-// //                 </Row>
-// //                 <Row className='mb-20'>
-// //                     <Button
-// //                         variant="primary"
-// //                         className='fade-in flex mx-auto items-center justify-center hover:scale-105 duration-300 rounded bg-gradient-to-b from-cyan-500 to-blue-500 lg:w-1/5 md:w-1/5 w-2/4 h-10 text-center cursor-pointer mt-10 gap-2'
-// //                         data-aos="zoom-in"
-// //                         data-aos-duration="500">
-// //                         <AiOutlineCloudDownload />
-// //                         <a href="/" download className="text-white">Download CV</a>
-// //                     </Button>
-// //                 </Row>
-// //             </Container>
-// //         </div>
-// //     );
-// // }
-
-// // export default About;
-
-
-
-// import React, { useEffect } from 'react';
-// import { AiOutlineCloudDownload } from 'react-icons/ai';
-// import { Container, Row, Col, Button } from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import AOS from 'aos';
-// import 'aos/dist/aos.css';
-// import '../Style.css';
-
-// const About = () => {
-//     useEffect(() => {
-//         AOS.init({ duration: 1000 });
-//     }, []);
-
-//     return (
-//         <div name="about" className='w-full h-auto bg-gradient-to-b from-gray-800 to-gray-900 text-white'>
-//             <Container fluid className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-auto'>
-//                 <Row className='pb-8'>
-//                     <h2 className='text-4xl font-bold inline border-b-4 border-gray-500'>About</h2>
-//                 </Row>
-
-//                 <Row>
-//                     <p data-aos="slide-up" data-aos-duration="500" className='text-xl mt-5 ease-in fade-in'>
-//                         I embarked my journey as a B.Tech Computer Science & Engineering student at Ramkrishna Mahato Government Engineering College in 2020. Along the way, I fearlessly explored various fields, including Graphic Designing, Content Creation, Freelancing, and Entrepreneurship. I delved into the world of cutting-edge technologies, such as Python, Cloud computing, React, Flutter, and Firebase. I also took my passion for technology and leadership to new heights by co-establishing Google Developer Student Clubs and the E-Cell in my college, alongside my equally enthusiastic classmates. At present, I serve GDSC RKMGEC as the Co-Lead and holds the significant responsibility of being the In-Charge of E-Cell RKMGEC.
-//                     </p>
-//                 </Row>
-//                 <Row>
-//                     <p data-aos="slide-up" data-aos-duration="500" className='text-xl mt-5 ease-in fade-in'>
-//                         In a constant pursuit of sharing knowledge and fostering growth, I've organized numerous technical workshops, imparting valuable insights on technologies like Python, Android, and Google Cloud to aspiring learners. With a commitment to personal development and a drive for making a positive impact within the tech community.
-//                     </p>
-//                 </Row>
-//                 <Row className='mb-20'>
-//                     <Button
-//                         variant="primary"
-//                         className='fade-in flex mx-auto items-center justify-center hover:scale-105 duration-300 rounded bg-gradient-to-b from-cyan-500 to-blue-500 lg:w-1/5 md:w-1/5 w-2/4 h-10 text-center cursor-pointer mt-10 gap-2'
-//                         data-aos="zoom-in"
-//                         data-aos-duration="500">
-//                         <AiOutlineCloudDownload />
-//                         <a href="/" download className="text-white">Download CV</a>
-//                     </Button>
-//                 </Row>
-//             </Container>
-//         </div>
-//     );
-// }
-
-// export default About;
-
 
 
 import React, { useEffect } from 'react';
@@ -141,14 +45,7 @@ database mongodb.
                     </p>
                 </Row>
                 <Row className='mb-20'>
-                    {/* <Button
-                        variant="primary"
-                        className='fade-in flex mx-auto items-center justify-center hover:scale-105 duration-300 rounded bg-gradient-to-b from-cyan-500 to-blue-500 lg:w-1/5 md:w-1/5 w-2/4 h-10 text-center cursor-pointer mt-10 gap-2'
-                        data-aos="zoom-in"
-                        data-aos-duration="500">
-                        <AiOutlineCloudDownload />
-                        <a href="/" download className="text-white">Download CV</a>
-                    </Button> */}
+                   
 
                     <Button
     variant="primary"
